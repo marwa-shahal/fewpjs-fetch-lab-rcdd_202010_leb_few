81c7ee301e5f061ff9cba1b584b9669bb2c61520
Marwa Sh <marwa.al.shahal@gmail.com>
2020-11-29 19:13:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function fetchBooks() {
-
+function fetchBooks ( ) {
+  return fetch('https://anapioficeandfire.com/api/books')
+      .then(res => res.json())
+      .then(json => {renderBooks(json);})
 }
-
+  
+  
 function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
